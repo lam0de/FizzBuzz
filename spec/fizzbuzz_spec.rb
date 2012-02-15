92b4207216a97645a6975f3f318e85b683736b24
Wolfgang Wiedenroth <wolfgang.wiedenroth@agilemanic.com>
2012-02-15 20:22:30 +0100

    update tests

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -2,27 +2,33 @@ require 'spec_helper'
 
 describe FizzBuzz do
   
-  describe "return value for numbers"
+  describe "return value for numbers" do
 
-      context "given a number multiple by 3"
+      context "given a number multiple by 3" do
         it "should return FIZZ" do
           FizzBuzz.print_fizz(3).should eq("FIZZ")
         end
+      end
 
-      context "given a number multiple by 5"
+      context "given a number multiple by 5" do
         it "should return BUZZ" do
           FizzBuzz.print_fizz(5).should eq("BUZZ")
         end
+      end
 
-      context "given a number multiple by 3 or 5"
+      context "given a number multiple by 3 or 5" do
         it "should return FIZZBUZZ" do
           FizzBuzz.print_fizz(15).should eq("FIZZBUZZ")
         end
+      end
 
-      context "given a number not multiple by 3 or 5"
+      context "given a number not multiple by 3 or 5" do
         it "should return the given value" do
           FizzBuzz.print_fizz(7).should eq(7)
         end
+      end
 
   end
+end
+
   
